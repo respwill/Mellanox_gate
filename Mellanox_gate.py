ec77d4ae3d3f3097ec88e2baf28554abcb763713
respwill <gonchogo@hotmail.com>
2017-01-24 09:45:18 +0900

    update for pre_split_fg tracking

--- Mellanox_gate.py
+++ Mellanox_gate.py
@@ -8,16 +8,16 @@ from HI_tool.schedule_check import sch_check
 
 # try:
 # creating Dataframe
-EL = sch_check("Mellanox(497) Package CES v1.2.xlsm","Turnkey schedule","Main Lot#/DCC", device_column="Main Tdevice",po_column="PO#",datecode_column="Date Code", current_fg_column="Main EL FG/PV")
+EL = sch_check("Mellanox(497) Package CES v1.2.xlsm","Turnkey schedule","Main Lot#/DCC", device_column="Main Tdevice",po_column="PO#",datecode_column="Date Code", current_fg_column="Main EL FG/PV", pre_split_fg_column = "Split EL FG/PV" )
 BE = sch_check("Mellanox(497) Package CES v1.2.xlsm","Normal BE schedule","Lot#/DCC", device_column="Tdevice",po_column="PO#", custInfo_column="Cust Info",tracecode_column="Trace Code(Date code in PO)", current_fg_column="FG",marking_spec_column="Spec#<MK#>")
 Return_BE = sch_check("Mellanox(497) Package CES v1.2.xlsm","Return BE schedule","Lot#/DCC", device_column="Main Tdevice",po_column="PO#", custInfo_column="Cust Info", tracecode_column="Trace Code(Date code in PO)", current_fg_column="FG", marking_spec_column="Spec#<MK#>")
 
 # import target lot# list from turnkey sheet.
 EL.set_target(497, "P/D/L", "PO Qty")
 EL.parser(EL.target_lots, EL.EMES_df,EL.result_df)
-EL.get_info(['test_device', 'test_po', 'date_code', 'current_fg'])
+EL.get_info(['test_device', 'test_po', 'date_code', 'current_fg', 'pre_split_fg'])
 EL.comparing('EL')
- q
+
 BE.set_target(497, "P/D/L", "PO Qty")
 BE.parser(BE.target_lots, BE.EMES_df,BE.result_df)
 BE.get_info(['test_device', 'test_po', 'cust_info', 'trace_code', 'current_fg', 'current_fg_marking'])
